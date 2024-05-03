8d33b719e78ba835affddbb17c3135bfc55761ab
Railtonofc <railtonaraujoofc@gmail.com>
2024-05-03 15:04:44 -0300

    atualizar

---
+++ Controle-Fluxo/src/CaixaEletronico.java
@@ -0,0 +1,14 @@
+public class CaixaEletronico {
+    
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 26.0;
+
+        if(valorSolicitado < saldo) {
+        saldo = saldo - valorSolicitado;
+        System.err.println("Novo saldo" + saldo);
+        }else
+        System.err.println("Saldo Insuficiente");
+
+    }
+}
